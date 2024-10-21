0dddd2e3e6aaaca885c56568ec88e03d71773f9e
brunovalu16 <brunodesigner3@gmail.com>
2024-10-21 09:42:54 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,14 @@
 
 
 const firebaseConfig = {
-    //Here, you can put your firebaseconfig
+  apiKey: "AIzaSyBn_sNe6UfJzZVQkZWTC-zSl1eoy78g0XE",
+  authDomain: "bancopowerbi.firebaseapp.com",
+  databaseURL: "https://bancopowerbi-default-rtdb.firebaseio.com",
+  projectId: "bancopowerbi",
+  storageBucket: "bancopowerbi.appspot.com",
+  messagingSenderId: "630219075006",
+  appId: "1:630219075006:web:9a0acd2446d0c5b8145def",
+  measurementId: "G-072KHB9PGE"
 };
 
 
