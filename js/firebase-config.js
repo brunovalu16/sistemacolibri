81a3856e4725f67a8c17c0f47b64f13e56a99103
brunovalu16 <brunodesigner3@gmail.com>
2024-10-21 09:20:24 -0300

    Update firebase-config.js
    take off the firebaseconfig.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,7 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBn_sNe6UfJzZVQkZWTC-zSl1eoy78g0XE",
-    authDomain: "bancopowerbi.firebaseapp.com",
-    projectId: "bancopowerbi",
-    storageBucket: "bancopowerbi.appspot.com",
-    messagingSenderId: "630219075006",
-    appId: "1:630219075006:web:9a0acd2446d0c5b8145def",
-    measurementId: "G-072KHB9PGE"
+    //Here, you can put your firebaseconfig
 };
 
 
